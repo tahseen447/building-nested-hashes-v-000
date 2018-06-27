3f51d7261d56c501761d9ef636c239ab054068fb
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-27 19:59:56 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,9 +5,7 @@ def third_challenge
         #your key/value pairs here
         },
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {
-        #your key/value pairs here
-        },
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    },
    :capulet => {
